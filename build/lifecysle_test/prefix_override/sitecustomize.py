1d7a5a5809577c420b4ccf03c73b79126f5ad2ed
itsuku-kito <129820033+itsuku-kito@users.noreply.github.com>
2025-11-27 17:10:02 +0900

    Delete build directory

--- build/lifecysle_test/prefix_override/sitecustomize.py
+++
@@ -1,4 +0,0 @@
-import sys
-if sys.prefix == '/usr':
-    sys.real_prefix = sys.prefix
-    sys.prefix = sys.exec_prefix = '/home/rsdlab/test_ws/install/lifecysle_test'
--- build/lifecysle_test/share/lifecysle_test/hook/pythonpath_develop.sh
+++
@@ -1,3 +0,0 @@
-# generated from colcon_core/shell/template/hook_prepend_value.sh.em
-
-_colcon_prepend_unique_value PYTHONPATH "/home/rsdlab/test_ws/build/lifecysle_test"
